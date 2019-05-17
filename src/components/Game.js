d4ab9ce5cd4b55ffb1b589a7a2224f8034eb58fe
Nick Worthington <nworthington16@gmail.com>
2019-05-16 22:14:15 -0400

    get scores from api and format dates

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,102 @@
 import React, { Component } from 'react';
 import './../style/Game.css';
 
+const gameStates = {
+    PRE: "pre",
+    IP: "ip",
+    FNL: "final",
+    DLY: "delayed",
+    PPD: "postponed"
+}
+
 class Game extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            status: gameStates.PRE,
+            awayScore: '-',
+            homeScore: '-'
+        }
+
+        this.formatTime = this.formatTime.bind(this);
+    }
+
+    componentDidMount() {
+        let status = this.props.details.status.detailedState;
+        let gameState;
+        if (status === 'Scheduled' || status === 'Pre-Game' || status === 'Warmup') {
+            gameState = gameStates.PRE;
+        } else if (status === 'Final') {
+            gameState = gameStates.FNL;
+        } else if (status === 'In Progress') {
+            gameState = gameStates.IP;
+        } else if (status === 'Delayed') {
+            gameState = gameStates.DLY;
+        } else if (status === 'Postponed') {
+            gameState = gameStates.PPD;
+        }
+        this.setState({
+            status: gameState
+        });
+    }
+
+    formatTime(gameDate) {
+        let gameTime = gameDate.substring(11, 19);
+        let hour = parseInt(gameTime.substring(0, 2));
+        hour = (hour + 8) % 12;
+        return hour + gameTime.substring(2, 5) + ' ET';
+        
+    }
+
     render() {
         return (
             <div className="game-main">
-                <div>
-                    {this.props.game.teams.away.team.name}
+                <div className="teams">
+                    <div>
+                        {this.props.details.teams.away.team.name}
+                    </div>
+                    <div>
+                        {this.props.details.teams.home.team.name}
+                    </div>
                 </div>
-                <div>
-                    {this.props.game.teams.home.team.name}
+                <div className="score">
+                    {
+                        this.state.status === gameStates.PRE &&
+                        <div>
+                            <div>-</div>
+                            <div>-</div>
+                        </div>
+                    }
+                    {
+                        (this.state.status === gameStates.IP || this.state.status === gameStates.FNL) &&
+                        <div>
+                            <div>{this.props.details.teams.away.score}</div>
+                            <div>{this.props.details.teams.home.score}</div>
+                        </div>
+                    }
+                </div>
+                <div className="status">
+                    {
+                        this.state.status === gameStates.PRE &&
+                        <div>{this.formatTime(this.props.details.gameDate)}</div>
+                    }
+                    {
+                        this.state.status === gameStates.IP &&
+                        <div>
+                            In progress
+                        </div>
+                    }
+                    {
+                        this.state.status === gameStates.DLY &&
+                        <div>Delayed</div>
+                    }
+                    {
+                        this.state.status === gameStates.FNL &&
+                        <div>Final</div>
+                    }
                 </div>
             </div>
-            
         )
     }
 }
--- src/components/GamesContainer.js
+++ src/components/GamesContainer.js
@@ -6,14 +6,12 @@ class GamesContainer extends Component {
     render() {
         let games = this.props.games.map(game => {
             return (
-                <Game
-                    game={game} />
+                <Game key={game.gamePk} details={game} />
             )
         });
 
         return (
             <div className="games-main">
-                Games ({this.props.games.length})
                 <div>
                     {games}
                 </div>
